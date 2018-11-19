3af878b86b40a13e7b503ca43c230c99611927e6
yaozd <tedijoteamo@163.com>
2018-11-19 11:14:52 +0800

    velocity-$filePath的作用域只能在当前default.vm模板中，不能跳到其他模板

--- com.yzd.frame.v2.root/.doc/README-Velocity.md
+++ com.yzd.frame.v2.root/.doc/README-Velocity.md
@@ -1,6 +1,8 @@
 -[Velocity Spring Boot Project](https://github.com/alibaba/velocity-spring-boot-project/blob/master/README_CN.md)
 -[velocity-spring-boot-sample](https://github.com/alibaba/velocity-spring-boot-project/blob/master/README_CN.md)
 -
-```
 
+```
+#set($filePath = $UrlHelperTool.getStaticFilePath())
+$filePath的作用域只能在当前default.vm模板中，不能跳到其他模板
 ```
--- com.yzd.frame.v2.root/1.1-com.yzd.web/src/main/java/com/yzd/web/ApplicationWeb.java
+++ com.yzd.frame.v2.root/1.1-com.yzd.web/src/main/java/com/yzd/web/ApplicationWeb.java
@@ -2,8 +2,10 @@ package com.yzd.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = "classpath:project.properties",ignoreResourceNotFound = true)
 public class ApplicationWeb {
     /**
      * Used when run as JAR
