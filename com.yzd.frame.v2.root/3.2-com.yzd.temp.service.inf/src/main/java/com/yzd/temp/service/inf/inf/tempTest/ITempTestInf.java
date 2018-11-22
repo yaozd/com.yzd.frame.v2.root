7b98dc70d55ad51565d44ffbd12f94cf44183f5e
yzd <tedijoteamo@163.com>
2018-11-22 21:21:44 +0800

    * 实际的dubbo接口定义的地方  * 此接口的访问权限为default,只对本包可见  * 不可使用public，防止接口适用。

--- com.yzd.frame.v2.root/3.2-com.yzd.temp.service.inf/src/main/java/com/yzd/temp/service/inf/inf/tempTest/ITempTestInf.java
+++ com.yzd.frame.v2.root/3.2-com.yzd.temp.service.inf/src/main/java/com/yzd/temp/service/inf/inf/tempTest/ITempTestInf.java
@@ -4,7 +4,9 @@ import com.yzd.temp.service.inf.dto.tempTest.TempTestDTO;
 
 /***
  * 实际的dubbo接口定义的地方
+ * 此接口的访问权限为default,只对本包可见
+ * 不可使用public，防止接口适用。
  */
-public interface ITempTestInf {
+interface ITempTestInf {
     int insertSelective(TempTestDTO record);
 }
