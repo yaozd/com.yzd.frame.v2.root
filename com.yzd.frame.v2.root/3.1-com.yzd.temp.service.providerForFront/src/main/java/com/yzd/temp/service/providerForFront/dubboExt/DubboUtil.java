e1465da0a407dc41f32e397289e4c3190729592a
yaozd <tedijoteamo@163.com>
2018-11-28 18:04:40 +0800

    项目打包时间截--timestamp

---
+++ com.yzd.frame.v2.root/3.1-com.yzd.temp.service.providerForFront/src/main/java/com/yzd/temp/service/providerForFront/dubboExt/DubboUtil.java
@@ -0,0 +1,91 @@
+package com.yzd.id.generator.service.provider.util.dubboExt;
+
+import com.alibaba.dubbo.common.utils.StringUtils;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Enumeration;
+import java.util.TimeZone;
+import java.util.jar.Attributes;
+import java.util.jar.JarFile;
+import java.util.jar.Manifest;
+
+@Slf4j
+public class DubboUtil {
+
+    /***
+     * dubbo-启动时属性设置
+     */
+    public static void initForRun(){
+        setLogInit();
+        setPackageTimestamp();
+    }
+    //设置dubbo的日志属性
+    private static void setLogInit() {
+        //解决dubbo找不到log4j
+        /* log4j:WARN No appenders could be found for logger (com.alibaba.dubbo.common.logger.LoggerFactory).
+        log4j:WARN Please initialize the log4j system properly.
+        log4j:WARN See http://logging.apache.org/log4j/1.2/faq.html#noconfig for more info*/
+        System.setProperty("dubbo.application.logger","slf4j");
+        //
+    }
+    //设置dubbo打包的版本号
+    private static String setPackageTimestamp() {
+        String packageTimestamp=getTimestamp();
+        if(StringUtils.isBlank(packageTimestamp)){
+            packageTimestamp="没有找到【当前非Maven打包后运行】";
+        }else {
+            System.setProperty("dubbo.package.timestamp", packageTimestamp);
+        }
+        log.info("项目打包时间截--timestamp:"+packageTimestamp);
+        return packageTimestamp;
+    }
+    //private static final String MANIFEST_DIRECTORY_LOCATION = "META-INF" + File.separator + "MANIFEST.MF";
+    private static final String MANIFEST_DIRECTORY_LOCATION = "META-INF/MANIFEST.MF";
+    private static String getTimestamp(){
+        getManifestInfo();
+     return null;
+    }
+
+    public static String getManifestInfo() {
+        String rootPath= DubboUtil.class.getProtectionDomain().getCodeSource().getLocation().getFile();
+
+        Enumeration resEnum;
+        try {
+            //resEnum = Thread.currentThread().getContextClassLoader().getResources(JarFile.MANIFEST_NAME);
+            resEnum = new MyResources().getClass().getClassLoader().getResources(JarFile.MANIFEST_NAME);
+            while (resEnum.hasMoreElements()) {
+                try {
+                    URL url = (URL)resEnum.nextElement();
+                    InputStream is = url.openStream();
+                    System.out.println(url.getFile());
+                    if (is != null) {
+                        Manifest manifest = new Manifest(is);
+                        Attributes mainAttribs = manifest.getMainAttributes();
+                        String version = mainAttribs.getValue("Timestamp");
+                        if(version != null) {
+                            System.out.println("======"+version);
+                            return version;
+                        }
+                    }
+                    if (is != null){is.close();}
+                }
+                catch (Exception e) {
+                    // Silently ignore wrong manifests on classpath?
+                }
+            }
+        } catch (IOException e1) {
+            // Silently ignore wrong manifests on classpath?
+        }
+        return null;
+    }
+    static class MyResources{}
+}
